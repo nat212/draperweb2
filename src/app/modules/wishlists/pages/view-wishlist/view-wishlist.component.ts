86566b22d8efb595076588154b83cf733d0b8ac4
Natasha Draper <natasha@draper.net.za>
2020-12-05 15:10:15 +0200

    Fix adding wishlist items

--- src/app/modules/wishlists/pages/view-wishlist/view-wishlist.component.ts
+++ src/app/modules/wishlists/pages/view-wishlist/view-wishlist.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { filterNil } from '@datorama/akita';
+import { RouterQuery } from '@datorama/akita-ng-router-store';
 import { WishlistItemDialogComponent } from '@modules/wishlists/dialogs/wishlist-item-dialog/wishlist-item-dialog.component';
 import { WishlistItem } from '@modules/wishlists/state/wishlist-item/wishlist-item.model';
 import { WishlistItemQuery } from '@modules/wishlists/state/wishlist-item/wishlist-item.query';
@@ -12,14 +13,18 @@ import { WishlistItemService } from '@modules/wishlists/state/wishlist-item/wish
   styleUrls: ['./view-wishlist.component.scss'],
 })
 export class ViewWishlistComponent implements OnInit {
+  private wishlistId: string;
+
   constructor(
     private readonly query: WishlistItemQuery,
     private readonly dialog: MatDialog,
     private readonly service: WishlistItemService,
+    private readonly routerQuery: RouterQuery,
   ) {}
 
   public ngOnInit() {
     this.query.selectAll().subscribe(console.log);
+    this.routerQuery.selectParams('wishlistId').subscribe((wishlistId: string) => (this.wishlistId = wishlistId));
   }
 
   public addWishlistItem() {
@@ -28,7 +33,7 @@ export class ViewWishlistComponent implements OnInit {
       .afterClosed()
       .pipe(filterNil)
       .subscribe((value: Partial<WishlistItem>) => {
-        this.service.add(value);
+        this.service.add(value, { params: { wishlistId: this.wishlistId } });
       });
   }
 }
