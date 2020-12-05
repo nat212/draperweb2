fab42708b051a6174a506609cf544fd5815cd251
Natasha Draper <natasha@draper.net.za>
2020-12-05 15:06:18 +0200

    Changes to update

--- src/app/modules/session/pages/home/home.component.ts
+++ src/app/modules/session/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UpdateService } from '@services/update.service';
 
 interface Link {
@@ -12,11 +12,15 @@ interface Link {
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   public links: Link[] = [
     { icon: 'request_quote', title: 'Budgets', path: 'budgets' },
     { icon: 'card_giftcard', title: 'Wishlists', path: 'wishlists' },
   ];
 
   constructor(public update: UpdateService) {}
+
+  public ngOnInit() {
+    this.update.subscribe();
+  }
 }
--- src/app/services/update.service.ts
+++ src/app/services/update.service.ts
@@ -2,7 +2,7 @@ import { ApplicationRef, Injectable } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
 import { concat, interval, Observable } from 'rxjs';
 import { first, map } from 'rxjs/operators';
-import { AlertService } from './alert.service';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -10,15 +10,17 @@ import { AlertService } from './alert.service';
 export class UpdateService {
   public updateAvailable$: Observable<boolean>;
 
-  constructor(private swUpdate: SwUpdate, private readonly app: ApplicationRef, private readonly alert: AlertService) {
+  constructor(private swUpdate: SwUpdate, private readonly app: ApplicationRef) {
     this.updateAvailable$ = this.swUpdate.available.pipe(map((update) => !!update));
   }
 
   public subscribe() {
-    const appIsStable$ = this.app.isStable.pipe(first((stable) => !!stable));
-    const every30Minutes$ = interval(30 * 1000 * 60);
-    const every30MinutesOnceAppIsStable$ = concat(appIsStable$, every30Minutes$);
-    every30MinutesOnceAppIsStable$.subscribe(() => this.swUpdate.checkForUpdate());
+    if (environment.production) {
+      const appIsStable$ = this.app.isStable.pipe(first((stable) => !!stable));
+      const every30Minutes$ = interval(30 * 1000 * 60);
+      const every30MinutesOnceAppIsStable$ = concat(appIsStable$, every30Minutes$);
+      every30MinutesOnceAppIsStable$.subscribe(() => this.swUpdate.checkForUpdate());
+    }
   }
 
   public activateUpdate() {
