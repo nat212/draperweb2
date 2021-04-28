19cf257f0a47acc56e7f4ea8c2847bd2cd73e7ff
Natasha Draper <natasha@quantsolutions.co.za>
2021-04-28 12:57:05 +0200

    Update firebase stuff

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyCgedakZReuyGfkrLM4pQaxK57BX6nd7BE',
-    authDomain: 'draperweb-2.firebaseapp.com',
-    databaseURL: 'https://draperweb-2.firebaseio.com',
-    projectId: 'draperweb-2',
-    storageBucket: 'draperweb-2.appspot.com',
-    messagingSenderId: '475657754543',
-    appId: '1:475657754543:web:5d683fc551bf4a80d6c15a',
-    measurementId: 'G-Y2P3E4NL10',
+    apiKey: 'AIzaSyBVdNuGIB-5EjzImB4C6dlBzF1ylaVBzPk',
+    authDomain: 'draperweb-1414d.firebaseapp.com',
+    projectId: 'draperweb-1414d',
+    storageBucket: 'draperweb-1414d.appspot.com',
+    messagingSenderId: '399233721775',
+    appId: '1:399233721775:web:4f4f7fa23a27a4c7157b5a',
   },
   mdiCodePointsURL: 'https://raw.githubusercontent.com/google/material-design-icons/master/font/MaterialIcons-Regular.codepoints',
 };
